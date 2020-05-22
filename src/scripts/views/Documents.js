56638609aff67c9af39b0ba51a15dee4224ee15d
Lucas García <lucasgarciabaro@gmail.com>
2020-05-22 18:53:46 +0200

    POST TUTORIAL

--- src/scripts/views/Documents.js
+++ src/scripts/views/Documents.js
@@ -2,12 +2,21 @@ const BaseView = require("../core/BaseView.js");
 
 
 const Documents = (function () {
-    const Documents = BaseView.extend(function (el, template) {
+
+    /// PRIVATE BLOCK CODE
+    var renderCount = 0;
+    /// END OF PRIVATE BLOCK CODE
+    
+    var Documents = function (el, template) {
         const self = this;
-        this.load("data/documents.json", function (response) {
+        this.load("data/documents.json").then(function (response) { 
+            // this == funció anonima
+            // self == Documents
             self.data = JSON.parse(response);
         });
-    });
+    };
+
+    Documents = BaseView.extend(Documents);
 
     Documents.prototype.onUpdate = function onUpdate () {
         console.log("Documents updated");
@@ -15,11 +24,34 @@ const Documents = (function () {
     }
 
     Documents.prototype.onRender = function onRender () {
+        const self = this;
+        for (let doc of self.el.querySelectorAll(".doc-row")) {
+            doc.addEventListener("click", self.onClickDocument);
+        }
+        // const list = document.createElement("ul");
+        // self.data.forEach(function (doc) {
+        //     var link = document.createElement("a");
+        //     link.href = "statics/data/" + doc.file;
+        //     link.setAttribute("target", "_blank");
+        //     var listElement = document.createElement("li");
+        //     listElement.innerText = doc.name;
+        //     listElement.setAttribute("data-file", doc.file);
+        //     link.appendChild(listElement);
+        //     list.appendChild(link);
+        // });
+        // this.el.appendChild(list);
         console.log("Documents rendered");
     }
 
     Documents.prototype.onRemove = function onRemove () {
+        for (let doc of self.el.querySelectorAll(".doc-row")) {
+            doc.removeEventListener("click", self.onClickDocument);
+        }
         console.log("Documents removed");
+    }
+
+    Documents.prototype.onClickDocument = function (ev) {
+        window.open("statics/data/" + ev.currentTarget.dataset.file);
     }
 
     return Documents;
---
+++ src/scripts/views/Gallery.js
@@ -0,0 +1,46 @@
+const BaseView = require("../core/BaseView.js");
+
+
+const Gallery = (function () {
+
+    /// PRIVATE BLOCK CODE
+    var renderCount = 0;
+    /// END OF PRIVATE BLOCK CODE
+    
+    var Gallery = function (el, template) {
+        const self = this;
+        this.load("data/gallery_images.json").then(function (response) { 
+            self.data = JSON.parse(response);
+        });
+    };
+
+    Gallery = BaseView.extend(Gallery);
+
+    Gallery.prototype.onUpdate = function onUpdate () {
+        console.log("Gallery updated");
+        this.render();
+    }
+
+    Gallery.prototype.onRender = function onRender () {
+        const self = this;
+        for (let img of self.el.querySelectorAll(".img-row")) {
+            img.addEventListener("click", self.onClickImage);
+        }
+        console.log("Gallery rendered");
+    }
+
+    Gallery.prototype.onRemove = function onRemove () {
+        for (let img of self.el.querySelectorAll(".img-row")) {
+            img.removeEventListener("click", self.onClickImage);
+        }
+        console.log("Gallery removed");
+    }
+
+    Gallery.prototype.onClickImage = function (ev) {
+        console.log("Has clicat sobre una imàtge!");
+    }
+
+    return Gallery;
+})();
+
+module.exports = Gallery;
