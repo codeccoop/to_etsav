76fc906315f00004defe98cd6e2f3fbf1098773e
Orzo Cogorzo <orzocogorzo@hotmail.com>
2020-10-21 15:28:59 +0200

    Load home.json to fill the Home section.

--- src/scripts/views/Home.js
+++ src/scripts/views/Home.js
@@ -4,20 +4,23 @@ const BaseView = require("../core/BaseView.js");
 const Home = (function () {
     const Home = BaseView.extend(function (el, template) {
         const self = this;
-        this.render();
+      this.load(_env.apiURL + "home.json").then(function (response) {
+        sefl.data = JSON.parse(response);
+      });
     });
 
     Home.prototype.onUpdate = function onUpdate () {
-        console.log("Home updated");
-    }
+      // console.log("Home updated");
+      this.render();
+    };
 
     Home.prototype.onRender = function onRender () {
         console.log("Home rendered");
-    }
+    };
 
     Home.prototype.onRemove = function onRemove () {
         console.log("Home removed");
-    }
+    };
 
     return Home;
 })();
