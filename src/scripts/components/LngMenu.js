abb45fee10123fb4dabc290628f93c0bb6f10de4
Orzo Cogorzo <orzocogorzo@hotmail.com>
2020-11-25 23:05:09 +0100

    Add touch compatibility to the language menu selector

--- src/scripts/components/LngMenu.js
+++ src/scripts/components/LngMenu.js
@@ -14,6 +14,8 @@ const LngMenu = (function () {
         }
         this.data.languages = languages;
         this.onClickItem = this.onClickItem.bind(this);
+        this.onClickVisible = this.onClickVisible.bind(this);
+        this.onClickOut = this.onClickOut.bind(this);
         this.render();
     });
 
@@ -24,12 +26,36 @@ const LngMenu = (function () {
         const currentLanguage = this.data.languages.filter(lng => {
             return lng.id == this.app.lng.currentLanguage;
         }).pop();
-        this.el.querySelector(".lng-menu__visible").innerText = currentLanguage.name;
+        const visible = this.el.querySelector(".lng-menu__visible");
+        visible.addEventListener("click", this.onClickVisible);
+        visible.innerText = currentLanguage.name;
     };
 
     LngMenu.prototype.onClickItem = function onClickItem (ev) {
-        this.el.querySelector(".lng-menu__visible").innerText = ev.currentTarget.innerText;
+        ev.stopPropagation();
+        ev.preventDefault();
+        this.el.classList.remove("expanded");
+        const visible = this.el.querySelector(".lng-menu__visible");
+        visible.addEventListener("click", this.onClickVisible);
+        visible.innerText = ev.currentTarget.innerText;
+        document.body.removeEventListener("click", this.onClickOut);
         this.app.lng.currentLanguage = ev.currentTarget.id;
+    };
+
+    LngMenu.prototype.onClickVisible = function onClickVisible (ev) {
+        ev.stopPropagation();
+        ev.preventDefault();
+        this.el.classList.add("expanded");
+        this.el.querySelector(".lng-menu__visible").removeEventListener("click", this.onClickVisible);
+        document.body.addEventListener("click", this.onClickOut);
+    };
+
+    LngMenu.prototype.onClickOut = function onClickOut (ev) {
+        ev.stopPropagation();
+        ev.preventDefault();
+        if (!this.el.contains(ev.currentTarget)) this.el.classList.remove("expanded");
+        document.body.removeEventListener("click", this.onClickOut);
+        this.el.querySelector(".lng-menu__visible").addEventListener("click", this.onClickVisible);
     };
 
     return LngMenu;
