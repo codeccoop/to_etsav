ac7acd0c21087a233be2d5e0f48c089840f4edd6
Orzo Cogorzo <orzocogorzo@hotmail.com>
2021-12-21 08:17:43 +0100

    Configure preproduction environ

--- envs.js
+++ envs.js
@@ -2,25 +2,25 @@
 // INFORMATION ABOUT HOW TO REACH THE SERVER
 
 module.exports = {
-    dev: {
-        name: 'development',
-        host: '127.0.0.1',
-        port: 8050,
-        apiURL: '/public/data/',
-        publicURL: '/public/'
-    },
-    pre: {
-        name: 'githubio',
-        host: 'https://orzocogorzo.github.io',
-        port: null,
-        apiURL: 'https://orzocogorzo.github.io/public/data/',
-        publicURL: 'https://orzocogorzo.github.io/public/'
-    },
-    pro: {
-        name: 'production',
-        host: "http://domain.com/path",
-        port: null,
-        apiURL: 'http://domain.com/path/rest/',
-        publicURL: 'http://domain.com/path/app/public/'
-    }
-}
+  dev: {
+    name: "development",
+    host: "127.0.0.1",
+    port: 8050,
+    apiURL: "/public/data/",
+    publicURL: "/public/",
+  },
+  pre: {
+    name: "githubio",
+    host: "https://orzocogorzo.github.io",
+    port: null,
+    apiURL: "https://dadescomunals.tk/to/public/data/",
+    publicURL: "https://dadescomunals.tk/to/public/",
+  },
+  pro: {
+    name: "production",
+    host: "http://domain.com/path",
+    port: null,
+    apiURL: "http://domain.com/path/rest/",
+    publicURL: "http://domain.com/path/app/public/",
+  },
+};
--- tablaorc.js
+++ tablaorc.js
@@ -2,17 +2,22 @@
 // ABOUT THE PROJECT
 
 module.exports = {
-    src: "src",
-    public: "public",
-    dist: "/home/orzo/github/githubio/",
-    port: 8050,
-    middleware: function (connect, opt) {
-        // Example middleware to disable cache on local environment.
-        return [function (req, res, next) {
-            res.setHeader("Cache-Control", "private, no-cache, no-store, must-revalidate");
-            res.setHeader("Pragme", "no-cache");
-            res.setHeader("Expires", "-1");
-            next();
-        }];
-    }
-}
+  src: "src",
+  public: "public",
+  dist: "dist",
+  port: 8050,
+  middleware: function (connect, opt) {
+    // Example middleware to disable cache on local environment.
+    return [
+      function (req, res, next) {
+        res.setHeader(
+          "Cache-Control",
+          "private, no-cache, no-store, must-revalidate"
+        );
+        res.setHeader("Pragme", "no-cache");
+        res.setHeader("Expires", "-1");
+        next();
+      },
+    ];
+  },
+};
